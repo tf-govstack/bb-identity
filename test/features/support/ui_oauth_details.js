4616e12204af7fe9db253333bb5f6429306678ef
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-01-30 17:55:46 +0100

    TECH-140: Fixed request responses and typos

--- test/features/support/ui_oauth_details.js
+++ test/features/support/ui_oauth_details.js
@@ -17,6 +17,64 @@ let invalidRedirectUri;
 
 const baseUrl = `${localhost}authorization/oauth-details`;
 
+const responseSchema = {
+  type: "object",
+  properties: {
+    responseTime: { type: "string" },
+    response: {
+      type: "object",
+      properties: {
+        transactionId: { type: "string" },
+        authFactors: {
+          type: "array",
+          items: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                type: {
+                  type: "string",
+                  enum: ["PIN", "OTP", "L1-bio-device", "Wallet"],
+                },
+                count: { type: "integer" },
+                bioSubTypes: { type: "array" },
+              },
+              required: ["type"],
+              additionalProperties: false,
+            },
+          },
+        },
+        essentialClaims: {
+          type: "array",
+          items: [
+            { type: "string" },
+            {
+              enum: [
+                "name",
+                "given_name",
+                "middle_name",
+                "preferred_username",
+                "nickname",
+                "gender",
+                "birthdate",
+                "email",
+                "phone_number",
+                "picture",
+                "address",
+              ],
+            },
+          ],
+        },
+      },
+      required: ["transactionId", "authFactors", "essentialClaims"],
+      additionalProperties: false,
+    },
+    errors: {},
+  },
+  required: ["response"],
+  additionalProperties: false,
+};
+
 const requestFunction = (scope, responseType, clientId, redirectUri) =>
   specUIOAuthDetails.post(baseUrl).withBody({
     requestTime: requestTime,
@@ -43,18 +101,33 @@ const requestFunction = (scope, responseType, clientId, redirectUri) =>
     },
   });
 
-const errorResultFunction = async (errorCode) => {
-  await specUIOAuthDetails.toss();
-  specUIOAuthDetails.response().should.have.status(400);
-  specUIOAuthDetails.response().should.have.jsonLike({
-    responseTime: "2022-09-22T08:03:45.287Z",
-    response: null,
-    errors: [
-      {
-        errorCode: errorCode,
+const resultFunction = async (withErrors = false) => {
+  if (!withErrors) {
+    responseSchema.properties.errors = { type: "null" };
+  } else {
+    responseSchema.properties.errors = {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          errorCode: {
+            type: "string",
+            enum: [
+              "invalid_client_id",
+              "invalid_redirect_uri",
+              "invalid_scope",
+              "no_acr_registered",
+              "unknown_error",
+            ],
+          },
+          errorMessage: { type: "string" },
+        },
       },
-    ],
-  });
+    };
+  }
+
+  specUIOAuthDetails.expectStatus(200).expectJsonSchema(responseSchema);
+  await specUIOAuthDetails.toss();
 };
 
 Before(() => {
@@ -67,7 +140,7 @@ Given(
   () => "Every provided query parameters are valid"
 );
 
-When("The user triggers an action with the required parameters", () =>
+When("The request with the required parameters is sent", () =>
   requestFunction(
     validScope,
     validResponseType,
@@ -77,23 +150,9 @@ When("The user triggers an action with the required parameters", () =>
 );
 
 Then(
-  "The user successfully validates the provided request parameters and resolve the required authentication factors",
+  "The user successfully validates the provided request parameters and resolves the required authentication factors",
   async () => {
-    await specUIOAuthDetails.toss(),
-      specUIOAuthDetails.response().should.have.status(200),
-      specUIOAuthDetails.response().should.have.jsonLike({
-        responseTime: "2022-09-22T08:03:45.287Z",
-        response: {
-          transactionId: "string",
-          authFactors: [
-            {
-              type: "PIN",
-            },
-          ],
-          essentialClaims: ["all claims"],
-        },
-        errors: [],
-      });
+    await resultFunction();
   }
 );
 
@@ -115,8 +174,8 @@ When(
 );
 
 Then(
-  "The result of an operation returns an error because of an invalid scope parameter provided",
-  async () => await errorResultFunction("invalid_scope")
+  "The result of an operation returns an error because an invalid scope parameter was specified",
+  async () => await resultFunction(true)
 );
 
 // Scenario: The user is not able to validate the provided request parameters and resolve the required authentication factors because of an invalid clientId parameter
@@ -135,8 +194,8 @@ When("The user triggers an action with an invalid clientId parameter", () =>
 );
 
 Then(
-  "The result of an operation returns an error, because of an invalid clientId provided",
-  async () => await errorResultFunction("invalid_client_id")
+  "The result of an operation returns an error because an invalid clientId was specified",
+  async () => await resultFunction(true)
 );
 
 // Scenario: The user is not able to validate the provided request parameters and resolve the required authentication factors because of an invalid responseType provided
@@ -155,8 +214,8 @@ When("The user triggers an action with an invalid responseType parameter", () =>
 );
 
 Then(
-  "The result of an operation returns an error because of an invalid responseType provided",
-  async () => await errorResultFunction("invalid_response_type")
+  "The result of an operation returns an error because an invalid responseType was specified",
+  async () => await resultFunction(true)
 );
 
 // Scenario: The user is not able to validate the provided request parameters and resolve the required authentication factors because of an invalid redirectUri provided
@@ -175,8 +234,8 @@ When("The user triggers an action with an invalid redirectUri parameter", () =>
 );
 
 Then(
-  "The result of an operation returns an error because of an invalid redirectUri provided",
-  async () => await errorResultFunction("invalid_redirect_uri")
+  "The result of an operation returns an error because an invalid redirectUri was specified",
+  async () => await resultFunction(true)
 );
 
 // Scenario: The user is not able to validate the provided request parameters and resolve the required authentication factors because none parameters provided
@@ -190,8 +249,8 @@ When("The user triggers an action without the required parameters", () =>
 );
 
 Then(
-  "The result of an operation returns an error because none required parameters provided",
-  async () => await errorResultFunction("invalid_body")
+  "The result of an operation returns an error because none required parameters was specified",
+  async () => await resultFunction(true)
 );
 
 After(() => {
