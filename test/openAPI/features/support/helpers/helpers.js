a93e230c7d30f02cfdc23de3c0b30eddceb690d5
Michal Dulko <mdulko@soldevelo.com>
2023-04-25 11:22:09 +0200

    TECH-138-FIX: Updated ticket, moved feature

--- test/openAPI/features/support/helpers/helpers.js
+++ test/openAPI/features/support/helpers/helpers.js
@@ -1,3 +1,42 @@
 module.exports = {
   localhost: 'http://localhost:3333/',
+  statusCode200: 200,
+  defaultResponseTime: 15000,
+  oidcWellKnownJWKSEndpoint: '.well-known/jwks.json',
+  oidcWellKnownJWKSResponseSchema: {
+    type: 'object',
+    properties: {
+      keys: {
+        type: 'array',
+        items: [
+          {
+            type: 'object',
+            properties: {
+              kid: { type: 'string' },
+              alg: {
+                type: 'string',
+                enum: ['RS256'],
+              },
+              use: {
+                type: 'string',
+                enum: ['sig'],
+              },
+              kty: {
+                type: 'string',
+                enum: ['RSA'],
+              },
+              e: { type: 'string' },
+              n: { type: 'string' },
+              status: {
+                type: 'string',
+                enum: ['ACTIVE', 'EXPIRED', 'NEXT'],
+              },
+              x5c: { type: 'string' },
+            },
+            required: ['kid', 'alg', 'use', 'kty', 'e', 'n'],
+          },
+        ],
+      },
+    },
+  },
 };
