3eea158ec42db6c5cc267db733d00c2a9ade4502
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-05-02 14:32:19 +0200

    TECH-482: Added smoke test

--- test/openAPI/features/client_update.feature
+++ test/openAPI/features/client_update.feature
@@ -1,16 +1,14 @@
+@method=PUT @endpoint=/client-mgmt/oidc-client/{client_id}
 Feature: API to update existing Open ID Connect (OIDC) client.
 
-  Update OIDC Client Endpoint.
-
-  Request endpoint: PUT /client-mgmt/oidc-client/{client_id}
-
-  Background:
+  @smoke 
+  Scenario: The client profile is successfully updated in the Open ID Connect (OIDC) smoke type test
     Given The user wants to update the client profile in the Open ID Connect (OIDC)
-
-  Scenario: The client profile is successfully updated in the Open ID Connect (OIDC)
-    When The user triggers an action to update the client profile in the Open ID Connect (OIDC)
-    Then The user successfully updated the client profile in the Open ID Connect (OIDC)
-
-  Scenario: The user is not able to update the client profile, because it does not exist in the Open ID Connect (OIDC)
-    When The user triggers an action to update the client profile in the Open ID Connect (OIDC) with client id whick does not exist
-    Then The result of an operation returns an error, because the client_id does not exist in the Open ID Connect (OIDC)
+    And The client profile with "e-health-service" as clientId is created
+    When User sends PUT request with given "e-health-service" as client_id parameter, "2011-10-05T14:48:00.000Z" as requestTime, "Health Service" as clientName, "active" as status, "http://example.com" as logoUri, "http://example-redirect.com" as redirectUris, "name" as userClaims, "idbb:acr:generated-code" as authContextRefs, "authorization_code" as grantTypes, "private_key_jwt" as clientAuthMethods
+    Then User receives a response from the PUT /client-mgmt/oidc-client/{client_id} endpoint
+    And The PUT /client-mgmt/oidc-client/{client_id} endpoint response should be returned in a timely manner 15000ms
+    And The PUT /client-mgmt/oidc-client/{client_id} endpoint response should have status 200
+    And The PUT /client-mgmt/oidc-client/{client_id} endpoint response should have content-type: application/json header
+    And The PUT /client-mgmt/oidc-client/{client_id} endpoint response should match json schema
+    And The PUT /client-mgmt/oidc-client/{client_id} endpoint response should contain "e-health-service" as clientId
--- test/openAPI/features/support/client_update.js
+++ test/openAPI/features/support/client_update.js
@@ -1,97 +1,164 @@
-const pactum = require("pactum");
-const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
-const { localhost } = require("./helpers/helpers");
+const chai = require('chai');
+const { spec } = require('pactum');
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+const {
+  localhost,
+  clientUpdateEndpoint,
+  clientCreateEndpoint,
+  defaultExpectedResponseTime,
+  contentTypeHeader,
+  clientResponseSchema,
+} = require('./helpers/helpers');
+
+chai.use(require('chai-json-schema'));
 
 let requestPayload;
+let specClientCreate;
 let specClientUpdate;
 let clientId;
 
-const baseUrl = `${localhost}client-mgmt/oidc-client/{client_id}`;
-const requestFunction = (clientId) =>
-  specClientUpdate
-    .put(baseUrl)
-    .withPathParams("client_id", clientId)
-    .withBody(requestPayload);
+const baseUrl = localhost + clientUpdateEndpoint;
+const endpointTag = { tags: `@endpoint=/${clientUpdateEndpoint}` };
 
-Before(() => {
-  specClientUpdate = pactum.spec();
+// const requestFunction = (clientId) =>
+//   specClientUpdate
+//     .put(baseUrl)
+//     .withPathParams('client_id', clientId)
+//     .withBody(requestPayload);
+
+Before(endpointTag, () => {
+  specClientCreate = spec();
+  specClientUpdate = spec();
 });
 
 // Background:
+// Given(
+//   'The user wants to update the client profile in the Open ID Connect \\(OIDC)',
+//   () => {
+//     requestPayload = {
+//       requestTime: '2022-09-22T08:03:45.000Z',
+//       request: {
+//         clientName: 'Health Service',
+//         status: 'active',
+//         logoUri: 'http://example.com',
+//         redirectUris: ['http://example.com'],
+//         userClaims: ['name'],
+//         authContextRefs: ['mosip:idp:acr:static-code'],
+//         grantTypes: ['authorization_code'],
+//         clientAuthMethods: ['private_key_jwt'],
+//       },
+//     };
+
+//     return requestPayload;
+//   }
+// );
+
+// Scenario: The client profile is successfully updated in the Open ID Connect (OIDC) smoke type test
 Given(
-  "The user wants to update the client profile in the Open ID Connect \\(OIDC)",
-  () => {
-    requestPayload = {
-      requestTime: "2022-09-22T08:03:45.000Z",
+  'The user wants to update the client profile in the Open ID Connect \\(OIDC)',
+  () =>
+    'The user wants to update the client profile in the Open ID Connect (OIDC)'
+);
+
+Given(
+  'The client profile with {string} as clientId is created',
+  async (clientId) => {
+    specClientCreate.post(localhost + clientCreateEndpoint).withJson({
+      requestTime: '2011-10-05T14:48:00.000Z',
       request: {
-        clientName: "Health Service",
-        status: "active",
-        logoUri: "http://example.com",
-        redirectUris: ["http://example.com"],
-        userClaims: ["name"],
-        authContextRefs: ["mosip:idp:acr:static-code"],
-        grantTypes: ["authorization_code"],
-        clientAuthMethods: ["private_key_jwt"],
+        clientId: clientId,
+        clientName: 'Health Service',
+        relyingPartyId: 'bharath-gov',
+        logoUri: 'http://example.com',
+        publicKey: {},
+        authContextRefs: ['idbb:acr:generated-code'],
+        userClaims: ['name'],
+        grantTypes: ['authorization_code'],
+        clientAuthMethods: ['private_key_jwt'],
       },
-    };
-
-    return requestPayload;
-  }
-);
-
-// Scenario: The client profile is successfully updated in the Open ID Connect (OIDC)
-When(
-  "The user triggers an action to update the client profile in the Open ID Connect \\(OIDC)",
-  () => {
-    clientId = "785b806d0e594657b05aabdb30fff8a4";
-    requestFunction(clientId);
-  }
-);
-
-Then(
-  "The user successfully updated the client profile in the Open ID Connect \\(OIDC)",
-  async () => {
-    await specClientUpdate.toss();
-    specClientUpdate.response().should.have.status(200);
-    specClientUpdate.response().should.have.jsonLike({
-      responseTime: "string",
-      response: {
-        clientId: "string",
-      },
-      errors: [],
     });
+    await specClientCreate.toss();
+    return (clientId = specClientCreate._response.json.response.clientId);
   }
 );
 
-// Scenario: The user is not able to update the client profile, because it does not exist in the Open ID Connect (OIDC)
 When(
-  "The user triggers an action to update the client profile in the Open ID Connect \\(OIDC) with client id whick does not exist",
-  () => {
-    clientId = "doesnotexist";
-    requestFunction(clientId);
-  }
-);
-
-Then(
-  "The result of an operation returns an error, because the client_id does not exist in the Open ID Connect \\(OIDC)",
-  async () => {
-    await specClientUpdate.toss();
-    specClientUpdate.response().should.have.status(404);
-    specClientUpdate.response().should.have.jsonLike({
-      responseTime: "string",
-      response: {
-        clientId: "string",
-      },
-      errors: [
-        {
-          errorCode: "client_id does not exist",
-          errorMessage: "string",
+  'User sends PUT request with given {string} as client_id parameter, {string} as requestTime, {string} as clientName, {string} as status, {string} as logoUri, {string} as redirectUris, {string} as userClaims, {string} as authContextRefs, {string} as grantTypes, {string} as clientAuthMethods',
+  (
+    client_id,
+    requestTime,
+    clientName,
+    status,
+    logoUri,
+    redirectUris,
+    userClaims,
+    authContextRefs,
+    grantTypes,
+    clientAuthMethods
+  ) =>
+    specClientUpdate
+      .put(baseUrl)
+      .withPathParams('client_id', client_id)
+      .withBody({
+        requestTime: requestTime,
+        request: {
+          clientName: clientName,
+          status: status,
+          logoUri: logoUri,
+          redirectUris: [redirectUris],
+          userClaims: [userClaims],
+          authContextRefs: [authContextRefs],
+          grantTypes: [grantTypes],
+          clientAuthMethods: [clientAuthMethods],
         },
-      ],
-    });
+      })
+);
+
+Then(
+  'User receives a response from the PUT \\/client-mgmt\\/oidc-client\\/\\{client_id} endpoint',
+  async () => await specClientUpdate.toss()
+);
+
+Then(
+  'The PUT \\/client-mgmt\\/oidc-client\\/\\{client_id} endpoint response should be returned in a timely manner 15000ms',
+  () =>
+    specClientUpdate
+      .response()
+      .to.have.responseTimeLessThan(defaultExpectedResponseTime)
+);
+
+Then(
+  'The PUT \\/client-mgmt\\/oidc-client\\/\\{client_id} endpoint response should have status 200',
+  () => specClientUpdate.response().to.have.status(200)
+);
+
+Then(
+  'The PUT \\/client-mgmt\\/oidc-client\\/\\{client_id} endpoint response should have content-type: application\\/json header',
+  () =>
+    specClientUpdate
+      .response()
+      .should.have.header(contentTypeHeader.key, contentTypeHeader.value)
+);
+
+Then(
+  'The PUT \\/client-mgmt\\/oidc-client\\/\\{client_id} endpoint response should match json schema',
+  () => {
+    clientResponseSchema.properties.response.errors = [];
+    chai
+      .expect(specClientUpdate._response.json)
+      .to.be.jsonSchema(clientResponseSchema);
   }
 );
 
-After(() => {
+Then(
+  'The PUT \\/client-mgmt\\/oidc-client\\/\\{client_id} endpoint response should contain {string} as clientId',
+  (clientId) =>
+    chai
+      .expect(specClientCreate._response.json.response.clientId)
+      .to.be.equal(clientId)
+);
+
+After(endpointTag, () => {
+  specClientCreate.end();
   specClientUpdate.end();
 });
