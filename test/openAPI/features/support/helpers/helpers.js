9e1a96bdf1b34ec204ea6ce0f3629f080803bb64
Michal Dulko <mdulko@soldevelo.com>
2023-05-11 01:30:43 +0200

    TECH-135: Updated ticket, moved feature

--- test/openAPI/features/support/helpers/helpers.js
+++ test/openAPI/features/support/helpers/helpers.js
@@ -1,3 +1,33 @@
 module.exports = {
   localhost: 'http://localhost:3333/',
+  contentTypeHeader: {
+    key: 'content-type',
+    value: 'application/json; charset=utf-8',
+  },
+  defaultExpectedResponseTime: 15000,
+  oauthTokenEndpoint: 'oauth/token',
+  //shares
+  clientResponseSchema: {
+    type: 'object',
+    properties: {
+      responseTime: { type: 'string' },
+      response: {
+        type: 'object',
+        properties: { clientId: { type: 'string' } },
+        errors: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              errorCode: {
+                type: 'string',
+              },
+              errorMessage: { type: 'string' },
+            },
+            additionalProperties: false,
+          },
+        },
+      },
+    },
+  },
 };
