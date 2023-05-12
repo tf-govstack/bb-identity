c00584068da121ba49f219bea1d8e2b8a6803966
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-05-12 15:08:29 +0200

    TECH-646: Added positive test

---
+++ test/openAPI/features/support/wallet_generate_link_code.js
@@ -0,0 +1,84 @@
+const chai = require('chai');
+const { spec } = require('pactum');
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+const {
+  localhost,
+  contentTypeHeader,
+  defaultExpectedResponseTime,
+  walletGenerateLinkCodeEndpoint,
+  X_XSRF_TOKEN,
+  transactionId,
+  walletGenerateLinkCodeResponseSchema,
+} = require('./helpers/helpers');
+
+chai.use(require('chai-json-schema'));
+
+let specWalletGenerateLinkCode;
+
+const baseUrl = localhost + walletGenerateLinkCodeEndpoint;
+const endpointTag = { tags: `@endpoint=/${walletGenerateLinkCodeEndpoint}` };
+
+Before(endpointTag, () => {
+  specWalletGenerateLinkCode = spec();
+});
+
+// Scenario: Successfully generate link code
+Given('Wants to generate link code', () => 'Wants to generate link code');
+
+When(
+  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, transactionId and {string} as requestTime',
+  (requestTime) =>
+    specWalletGenerateLinkCode
+      .post(baseUrl)
+      .withHeaders(X_XSRF_TOKEN)
+      .withJson({
+        requestTime: requestTime,
+        request: {
+          transactionId: transactionId,
+        },
+      })
+);
+
+Then(
+  'Receive a response from the \\/linked-authorization\\/link-code endpoint',
+  async () => await specWalletGenerateLinkCode.toss()
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response should be returned in a timely manner 15000ms',
+  () =>
+    specWalletGenerateLinkCode
+      .response()
+      .to.have.responseTimeLessThan(defaultExpectedResponseTime)
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response should have status 200',
+  () => specWalletGenerateLinkCode.response().to.have.status(200)
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response should have content-type: application/json header',
+  () =>
+    specWalletGenerateLinkCode
+      .response()
+      .should.have.header(contentTypeHeader.key, contentTypeHeader.value)
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response should match json schema',
+  () => {
+    walletGenerateLinkCodeResponseSchema.properties.errors = [];
+    chai
+      .expect(specWalletGenerateLinkCode._response.json)
+      .to.be.jsonSchema(walletGenerateLinkCodeResponseSchema);
+  }
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code response should contain {string} property equals provided transactionId',
+  () =>
+    chai
+      .expect(specWalletGenerateLinkCode._response.json.response.transactionId)
+      .to.be.equal(transactionId)
+);
---
+++ test/openAPI/features/wallet_generate_link_code.feature
@@ -0,0 +1,13 @@
+@method=POST @endpoint=/linked-authorization/link-code
+Feature: The endpoint to generate link code.
+
+  @smoke @unit @positive
+  Scenario: Successfully generate link code
+    Given Wants to generate link code
+    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, transactionId and "2011-10-05T14:48:00.000Z" as requestTime
+    Then Receive a response from the /linked-authorization/link-code endpoint
+    And The /linked-authorization/link-code endpoint response should be returned in a timely manner 15000ms
+    And The /linked-authorization/link-code endpoint response should have status 200
+    And The /linked-authorization/link-code endpoint response should have content-type: application/json header
+    And The /linked-authorization/link-code endpoint response should match json schema
+    And The /linked-authorization/link-code response should contain transactionId property equals provided transactionId
