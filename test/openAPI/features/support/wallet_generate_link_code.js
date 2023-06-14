bb79005bdd66d7f99beac50df6b11deed928481f
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-06-14 14:01:54 +0200

    TECH-725: Changed test case for reused transactionId

--- test/openAPI/features/support/wallet_generate_link_code.js
+++ test/openAPI/features/support/wallet_generate_link_code.js
@@ -9,17 +9,28 @@ const {
   X_XSRF_TOKEN,
   transactionId,
   walletGenerateLinkCodeResponseSchema,
+  walletLinkTransactionEndpoint,
+  walletGenerateLinkAuthCodeEndpoint,
+  walletConsentEndpoint,
 } = require('./helpers/helpers');
 
 chai.use(require('chai-json-schema'));
 
 let specWalletGenerateLinkCode;
+let specWalletLinkTransaction;
+let specWalletGenerateLinkAuthCode;
+let specWalletConsent;
+let specWalletGenerateLinkCodeReusad;
 
 const baseUrl = localhost + walletGenerateLinkCodeEndpoint;
 const endpointTag = { tags: `@endpoint=/${walletGenerateLinkCodeEndpoint}` };
 
 Before(endpointTag, () => {
   specWalletGenerateLinkCode = spec();
+  specWalletLinkTransaction = spec();
+  specWalletGenerateLinkAuthCode = spec();
+  specWalletConsent = spec();
+  specWalletGenerateLinkCodeReusad = spec();
 });
 
 // Scenario: Successfully generates link code smoke type test
@@ -87,14 +98,14 @@ Then(
 // Scenario: Not able to generate link code because of a random transaction_id
 // Given and others Then for this scenario are written in the aforementioned example
 When(
-  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, random transactionId and {string} as requestTime',
-  (requestTime) =>
+  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, random transactionId and requestTime',
+  () =>
     specWalletGenerateLinkCode
       .post(baseUrl)
       .withHeaders(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
       .withCookies(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
       .withJson({
-        requestTime: requestTime,
+        requestTime: new Date().toISOString(),
         request: {
           transactionId: 'random-transaction-id',
         },
@@ -127,14 +138,14 @@ Then(
 // Scenario: Not able to generate link code because of a blank transaction_id
 // Given and others Then for this scenario are written in the aforementioned example
 When(
-  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, blank transactionId and {string} as requestTime',
-  (requestTime) =>
+  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, blank transactionId and requestTime',
+  () =>
     specWalletGenerateLinkCode
       .post(baseUrl)
       .withHeaders(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
       .withCookies(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
       .withJson({
-        requestTime: requestTime,
+        requestTime: new Date().toISOString(),
         request: {
           transactionId: '',
         },
@@ -143,19 +154,105 @@ When(
 
 // Scenario: Not able to generate link code because of reause of the completed transaction_id
 // Given and others Then for this scenario are written in the aforementioned example
+Given('The first authorization flow for transactionId ends', async () => {
+  specWalletGenerateLinkCode
+    .post(baseUrl)
+    .withHeaders(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
+    .withCookies(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
+    .withJson({
+      requestTime: new Date().toISOString(),
+      request: {
+        transactionId: 'transaction_id_003',
+      },
+    });
+
+  await specWalletGenerateLinkCode.toss();
+
+  await specWalletLinkTransaction
+    .post(localhost + walletLinkTransactionEndpoint)
+    .withJson({
+      requestTime: new Date().toISOString(),
+      request: {
+        linkCode: specWalletGenerateLinkCode._response.json.response.linkCode,
+      },
+    });
+
+  await specWalletLinkTransaction.toss();
+
+  await specWalletConsent.post(localhost + walletConsentEndpoint).withJson({
+    requestTime: new Date().toISOString(),
+    request: {
+      linkedTransactionId:
+        specWalletLinkTransaction._response.json.response.linkTransactionId,
+    },
+  });
+
+  await specWalletGenerateLinkAuthCode
+    .post(localhost + walletGenerateLinkAuthCodeEndpoint)
+    .withHeaders(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
+    .withJson({
+      requestTime: new Date().toISOString(),
+      request: {
+        linkedCode: specWalletGenerateLinkCode._response.json.response.linkCode,
+        transactionId: 'transaction_id_003',
+      },
+    });
+});
+
 When(
-  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, reaused completed transactionId and {string} as requestTime',
-  (requestTime) =>
-    specWalletGenerateLinkCode
+  'Send POST \\/linked-authorization\\/link-code request with given X-XSRF-TOKEN header, reaused completed transactionId and requestTime',
+  () =>
+    specWalletGenerateLinkCodeReusad
       .post(baseUrl)
       .withHeaders(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
       .withCookies(X_XSRF_TOKEN.key, X_XSRF_TOKEN.value)
       .withJson({
-        requestTime: requestTime,
+        requestTime: new Date().toISOString(),
         request: {
-          transactionId: 'reused_completed_transaction_id',
+          transactionId: 'transaction_id_003',
         },
       })
+);
+
+Then(
+  'Receive a response for completed transactionId from the \\/linked-authorization\\/link-code endpoint',
+  async () => await specWalletGenerateLinkCodeReusad.toss()
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response for completed transactionId should have status 200',
+  () => specWalletGenerateLinkCodeReusad.response().to.have.status(200)
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response for completed transactionId should have content-type: application\\/json header',
+  () =>
+    specWalletGenerateLinkCodeReusad
+      .response()
+      .should.have.header(contentTypeHeader.key, contentTypeHeader.value)
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code endpoint response for completed transactionId should match json schema with errors',
+  () => {
+    chai
+      .expect(specWalletGenerateLinkCodeReusad._response.json)
+      .to.be.jsonSchema(walletGenerateLinkCodeResponseSchema);
+    chai.expect(specWalletGenerateLinkCodeReusad._response.json.errors).to.not
+      .be.empty;
+  }
+);
+
+Then(
+  'The \\/linked-authorization\\/link-code response for completed transactionId should contain errorCode property equals to {string}',
+  (errorCode) =>
+    chai
+      .expect(
+        specWalletGenerateLinkCodeReusad._response.json.errors
+          .map((error) => error.errorCode)
+          .toString()
+      )
+      .to.be.equal(errorCode)
 );
 
 // Scenario: Not able to generate link code because of invalid requestTime
@@ -199,4 +296,8 @@ Then(
 
 After(endpointTag, () => {
   specWalletGenerateLinkCode.end();
+  specWalletLinkTransaction.end();
+  specWalletGenerateLinkAuthCode.end();
+  specWalletConsent.end();
+  specWalletGenerateLinkCodeReusad.end();
 });
--- test/openAPI/features/wallet_generate_link_code.feature
+++ test/openAPI/features/wallet_generate_link_code.feature
@@ -15,7 +15,7 @@ Feature: The endpoint to generate link code.
   @negative
   Scenario: Not able to generate link code because of a random transaction_id
     Given Wants to generate link code
-    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, random transactionId and "2011-10-05T14:48:00.000Z" as requestTime
+    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, random transactionId and requestTime
     Then Receive a response from the /linked-authorization/link-code endpoint
     And The /linked-authorization/link-code endpoint response should be returned in a timely manner 15000ms
     And The /linked-authorization/link-code endpoint response should have status 200
@@ -26,7 +26,7 @@ Feature: The endpoint to generate link code.
   @negative
   Scenario: Not able to generate link code because of a blank transaction_id
     Given Wants to generate link code
-    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, blank transactionId and "2011-10-05T14:48:00.000Z" as requestTime
+    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, blank transactionId and requestTime
     Then Receive a response from the /linked-authorization/link-code endpoint
     And The /linked-authorization/link-code endpoint response should be returned in a timely manner 15000ms
     And The /linked-authorization/link-code endpoint response should have status 200
@@ -37,13 +37,13 @@ Feature: The endpoint to generate link code.
   @negative
   Scenario: Not able to generate link code because of reause of the completed transaction_id
     Given Wants to generate link code
-    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, reaused completed transactionId and "2011-10-05T14:48:00.000Z" as requestTime
-    Then Receive a response from the /linked-authorization/link-code endpoint
-    And The /linked-authorization/link-code endpoint response should be returned in a timely manner 15000ms
-    And The /linked-authorization/link-code endpoint response should have status 200
-    And The /linked-authorization/link-code endpoint response should have content-type: application/json header
-    And The /linked-authorization/link-code endpoint response should match json schema with errors
-    And The /linked-authorization/link-code response should contain errorCode property equals to "invalid_transaction_id"
+    And The first authorization flow for transactionId ends 
+    When Send POST /linked-authorization/link-code request with given X-XSRF-TOKEN header, reaused completed transactionId and requestTime
+    Then Receive a response for completed transactionId from the /linked-authorization/link-code endpoint
+    And The /linked-authorization/link-code endpoint response for completed transactionId should have status 200
+    And The /linked-authorization/link-code endpoint response for completed transactionId should have content-type: application/json header
+    And The /linked-authorization/link-code endpoint response for completed transactionId should match json schema with errors
+    And The /linked-authorization/link-code response for completed transactionId should contain errorCode property equals to "invalid_transaction_id"
 
   @unit @negative
   Scenario: Not able to generate link code because of invalid requestTime
